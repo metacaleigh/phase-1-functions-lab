6bcde98dd13c3f8bcc41e3078c8242a3a8f358c7
caleigh <caleighsteill@outlook.com>
2022-12-19 10:24:12 -0700

    commit tree 011d55960e313822fa4c76a1e86cdc85a051e83f

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if (someValue >= 42) {
+        return someValue - 42
+    } else if (someValue <= 42) {
+        return 42 - someValue
+    }
+}
+
+distanceFromHqInBlocks(43)
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue)*264
+}
+
+distanceFromHqInFeet(43)
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((destination - start)*264)
+}
+
+distanceTravelledInFeet(43, 48)
+
+let distanceInFeet = Math.abs((destination - start)*264)
+
+function calculatesFarePrice(start, destination) {
+    if (Math.abs((destination - start)*264) <= 400) {
+        return 0
+        } else if (Math.abs((destination - start)*264) < 2000) {
+            return (Math.abs((destination - start)*264)-400)*0.02
+        } else if (Math.abs((destination - start)*264) > 2000) {
+            if (Math.abs((destination - start)*264) > 2500) {
+                return "cannot travel that far"
+            }
+            return 25
+        }
+}
+
+calculatesFarePrice(43, 48)
+
+
